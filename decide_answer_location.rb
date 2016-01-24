abceeed8b9efbc5e86dda4e2abc35f355a02f3da
datake <sigur.ku@gmail.com>
2016-01-25 07:39:33 +0900

    答えの場所作成

---
+++ decide_answer_location.rb
@@ -0,0 +1,149 @@
+require 'csv'
+require 'pp'
+require 'logger'
+require 'set'
+require 'unf'
+
+def main
+  decide_answer_location
+end
+class Array
+  # 要素の平均を算出する
+  def avg
+    inject(0.0){|r,i| r+=i }/size
+  end
+  # 要素の分散を算出する
+  def variance
+    a = avg
+    inject(0.0){|r,i| r+=(i-a)**2 }/size
+  end
+  # 標準偏差を算出する
+  def standard_deviation
+    Math.sqrt(variance)
+  end
+end
+def split_comma_to_array (text)
+  # text=text.gsub(/"/, '')
+  lang_arr=text.split(",")
+  return lang_arr
+end
+class Transgraph
+  def initialize(input_filename)
+    @pivot = {}
+    @lang_a_b = {}
+    @lang_b_a = {}
+    @lang_a_p = {}
+    @lang_b_p = {}
+    @lang_p_a = {}
+    @lang_p_b = {}
+    @node_a = Set.new
+    @node_b = Set.new
+    CSV.foreach(input_filename) do |row|
+      if row.size==3
+        array_of_a = split_comma_to_array(row[1])
+        # if row[2]
+        array_of_b  = split_comma_to_array(row[2])
+        # pp array_of_b
+        @pivot[row[0]]=[array_of_a,array_of_b] #{"pivot"=>[[a1,a2,a3,..], [b1,b2,b3,..]]}
+
+        @lang_p_a[row[0]] = array_of_a
+        @lang_p_b[row[0]] = array_of_b
+        array_of_a.each{|a|
+          array_of_b.each{|b|
+            @lang_a_b[a]=array_of_b #{"a1"=>[b1,b2,b3,..]}とか{"a2"=>[b1,b2,b3,..]}
+            @lang_b_a[b]=array_of_a #{"b1"=>"a1,a2,a3,..]"}
+            #aやbからみたとき、複数のpivotが対応することがある
+            if @lang_a_p.has_key?(a)
+              @lang_a_p[a] << row[0] #{"a1"=>Set[pivot1,pivot2,..]}
+              # pp @lang_a_p[a]
+            else
+              @lang_a_p[a]=Set[row[0]] #{"a1"=>Set[pivot]}
+            end
+            if @lang_b_p.has_key?(b)
+              @lang_b_p[b] << row[0] #{"b1"=>Set[pivot1,pivot2,..]}
+              # pp @lang_b_p[b]
+            else
+              @lang_b_p[b]=Set[row[0]] #{"b1"=>"Set[pivot]}
+              # pp @lang_b_p[b][0]
+
+            end
+          }
+        }
+        array_of_a.each{|a|
+          @node_a<<a.to_s
+        }
+        array_of_b.each{|b|
+          @node_b<<b.to_s
+        }
+      end
+    end
+  end
+  attr_accessor :pivot
+  attr_accessor :lang_a_b
+  attr_accessor :lang_b_a
+  attr_accessor :lang_a_p
+  attr_accessor :lang_b_p
+  attr_accessor :lang_p_a
+  attr_accessor :lang_p_b
+  attr_accessor :node_a
+  attr_accessor :node_b
+end
+
+class Answer
+  def initialize(answer_filename)
+    @answer = {}
+    # @answer_head_trans = {}
+    @answer_head_trans = Hash.new {|h,k| h[k]=[]}
+    CSV.foreach(answer_filename) do |row|
+      @answer[row[0]]=row[1..-1]
+      if row[0 .. -1].size>1
+        row[1..-1].each{|trans|
+          if answer_head_trans.has_key?(trans)
+            answer_head_trans[trans] << row[0]
+            #まだ登録されていないkeyならvalueに追加
+          else
+            answer_head_trans[trans] << row[0]
+          end
+        }
+      end
+    end
+  end
+  attr_accessor :answer
+  attr_accessor :answer_head_trans
+end
+
+def decide_answer_location
+  languages = ["1-2","1-5","1-8","2-0"]
+    languages.each{|language|
+      output_filename="answer/type1/"+language+".csv"
+
+      File.open(output_filename, "a") do |io_out|
+
+      # pivot_connected_fixed=2 #ピボット共有率を計測する際の分母(繋がっているノード数)を指定
+      output_folder="newfeature/"
+      min=0
+
+      max=999
+      input_filename="partition/"+language+"/"
+
+      all_trans_sr_standardized=Array.new
+      min.upto(max) do |transgraph_itr|
+        pp input_filename+"#{transgraph_itr}.csv"
+        transgraph = Transgraph.new(input_filename+"#{transgraph_itr}.csv")
+
+        if transgraph.node_a.size > transgraph.node_b.size
+          transgraph.node_b.each_with_index {|node_b, idx|
+            io_out.puts("#{transgraph_itr}-a-#{idx},#{transgraph_itr}-b-#{idx}")
+          }
+        else
+          transgraph.node_a.each_with_index {|node_a, idx|
+            io_out.puts("#{transgraph_itr}-a-#{idx},#{transgraph_itr}-b-#{idx}")
+          }
+        end
+      end
+    end
+  }
+
+end
+
+main
